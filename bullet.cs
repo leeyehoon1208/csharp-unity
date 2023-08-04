cffbb9b8e393d44477ef8412347334ff3bc8c5f4
leeyehoon1208 <127827916+leeyehoon1208@users.noreply.github.com>
2023-08-04 14:41:20 +0900

    Update bullet.cs

--- bullet.cs
+++ bullet.cs
@@ -3,12 +3,14 @@ using UnityEngine;
 public class bullet : MonoBehaviour
 {
     public GameObject BulletPrefab;
+    public float BulletSpeed = 100f;
     void Update()
     {
-         if (Input.GetKeyDown(KeyCode.Space)) // space를 누르면 총알이 나온다 
+         if (Input.GetKeyDown(KeyCode.Space))
     {
-        Instantiate(BulletPrefab); // 총알이 처음 생성된다. (첫 위치) ) 미리 만들어놓은 총알 프리팹을 복제해서 사용하는 것이다. 
-
+        GameObject bullet = Instantiate(BulletPrefab); // 총알이 처음 생성된다. (첫 위치) ) 미리 만들어놓은 총알 프리팹을 복제해서 사용하는 것이다. 
+        bullet.transform.position = transform.position; // 총알의 위치를 (0,0)
+        bullet.GetComponent<Rigidbody2D>().AddForce(Vector2.up * BulletSpeed); // (0,1) * 100 = (0,100) 
     }
     }
 }
